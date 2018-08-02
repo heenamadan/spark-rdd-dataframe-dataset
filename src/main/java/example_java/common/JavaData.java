5c11e02eef0bb36155b8369d798f9d638f02fcd8
Heena Madan <Heena.Madan@akqa.com>
2018-08-02 15:36:28 +0530

    first commit

---
+++ src/main/java/example_java/common/JavaData.java
@@ -0,0 +1,41 @@
+package example_java.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaData {
+
+    public static List<JavaPerson> sampleData() throws IOException {
+        return sampleDataAsStrings().stream().map(line -> {
+            String[] parts = line.split(",");
+            return new JavaPerson(parts[0], parts[1], getAge(parts[2]), parts[3]);
+        }).collect(Collectors.toList());
+    }
+
+    public static List<String> sampleDataAsStrings() throws IOException {
+        List list = new ArrayList<String>();
+        BufferedReader br = new BufferedReader(new FileReader(new File("src/main/resources/sample_data.csv")));
+        String line;
+        while ((line=br.readLine())!=null) {
+            list.add(line);
+        }
+        br.close();
+        return list;
+    }
+
+
+    private static int getAge(String ageVal){
+        if (StringUtils.isNotEmpty(ageVal)) {
+            return Integer.parseInt(ageVal);
+        }
+        return 0;
+
+    }
+}
